61cac9764a1d47c1114f7093b18e4642296e132b
dlgusrb3456 <dlgusrb3456@naver.com>
2023-06-23 14:39:43 +0900

    main에서 수행되던 Computer 관련 로직으로 Computer class 생성 후 Computer 내부 로직으로 이동
    setAnswer 이름을 calcAnswer로 변경

--- baseball/src/main/java/org/baseball/Main.java
+++ baseball/src/main/java/org/baseball/Main.java
@@ -1,26 +1,9 @@
 package org.baseball;
 
-import java.util.Random;
-
 public class Main {
 
-  private static String answer;
-
   public static void main(String[] args) {
-
-    answer = setAnswer();
-  }
-
-  private static String setAnswer() {
-    Random random = new Random();
-    String stringAnswer = "";
-    while (stringAnswer.length() != 3) {
-      String tempAnswer = String.valueOf(random.nextInt(9) + 1);
-      if (!stringAnswer.contains(tempAnswer)) {
-        stringAnswer += tempAnswer;
-      }
-    }
-
-    return stringAnswer;
+    Computer computer = new Computer();
+    computer.calcAnswer();
   }
 }
