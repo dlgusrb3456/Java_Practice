1169a744e23c823bf3d314e3a70a95895c790916
dlgusrb3456 <dlgusrb3456@naver.com>
2023-06-23 14:50:06 +0900

    Player 객체 생성및 Player가 answer를 input하는 로직 구현

---
+++ baseball/src/main/java/org/baseball/Player.java
@@ -0,0 +1,15 @@
+package org.baseball;
+
+import java.util.Scanner;
+
+public class Player {
+
+  public Player() {
+
+  }
+
+  public int inputAnswer() {
+    Scanner scanner = new Scanner(System.in);
+    return scanner.nextInt();
+  }
+}
