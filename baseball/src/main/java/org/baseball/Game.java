78eb28a18fdebdd783c34da816474cffd714683f
dlgusrb3456 <dlgusrb3456@naver.com>
2023-06-25 18:37:03 +0900

    inputAnswer 호출시 Scanner를 인자로 넘김

--- baseball/src/main/java/org/baseball/Game.java
+++ baseball/src/main/java/org/baseball/Game.java
@@ -25,7 +25,8 @@ public class Game {
     computer.calculateAnswer();
 
     while (true) {
-      Result result = computer.checkAnswer(player.inputAnswer());
+      player.inputAnswer(new Scanner(System.in));
+      Result result = computer.checkAnswer(player.getPlayerInput());
       System.out.println(result.toString());
       if (result.getCorrect()) {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
